3229dc432cdbc34f2e281e9374ce9732fd523ac6
moults31 <moults31@gmail.com>
2017-08-06 23:05:30 -0700

    Implement basic PRAW scraping

---
+++ README.md
@@ -0,0 +1,7 @@
+AutoMusicLink
+
+This is a Reddit bot.
+
+Finds music video submissions on music-themed subreddits. 
+Comments a link to popular streaming services in order to 
+allow users to add this song to their queue or playlist.
--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,39 @@
 import praw
 import os
+import pkg_resources
 
-login_info = [os.environ['REDDIT_USERNAME'], os.environ['REDDIT_PASSWORD']]
-client_info = [os.environ['REDDIT_APP_ID'], os.environ['REDDIT_APP_SECRET']]
-user_agent = 'heroku:automusiclink:v1 (by /u/moults31)'
 
-reddit = praw.Reddit(client_id=os.environ['REDDIT_APP_ID'],
-                     client_secret=os.environ['REDDIT_APP_SECRET'],
-                     user_agent='heroku:automusiclink:v1 (by /u/moults31)',
-                     username=os.environ['REDDIT_USERNAME'],
-                     password=os.environ['REDDIT_PASSWORD'])
+# Return a list of reddit submission objects
+def getRedditPosts():
+    redditposts = list()
 
-print(reddit.read_only)
+    # Obtain a reddit instance
+    reddit = praw.Reddit(client_id=os.environ['REDDIT_APP_ID'],
+                        client_secret=os.environ['REDDIT_APP_SECRET'],
+                        user_agent='heroku:automusiclink:v1 (by /u/moults31)',
+                        username=os.environ['REDDIT_USERNAME'],
+                        password=os.environ['REDDIT_PASSWORD'])
+        
+    # Load list of subreddits to run in
+    listfile = pkg_resources.resource_filename(__name__, "../include/subredditlist.txt")
+    with open(listfile) as f:
+        subredditlist = f.readlines()
+        
+    for entry in subredditlist:
+        subreddit = reddit.subreddit(str.strip(entry))
+
+        print(subreddit.display_name)  # Output: redditdev
+
+        # Add this entry to output list.
+        # @TODO Filter out non-music posts
+        for submission in subreddit.hot(limit=10):
+            print(submission.title)  
+            redditposts.append(submission)
+            
+    return redditposts
+    
+                                    
+# Main
+
+redditposts = getRedditPosts()
+print(redditposts)
