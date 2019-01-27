a5a574379ffcb0a54b736bdc9df58dec743dcfd6
moults31 <moults31@gmail.com>
2019-01-26 16:30:26 -0800

    Add applemusic.py

--- src/applemusic.py
+++ src/applemusic.py
@@ -10,8 +10,17 @@ class AppleMusic:
     def __init__(self):
         self.client = AppleMusicClient( os.environ['APPLEMUSIC_TEAMID'],
                                         os.environ['APPLEMUSIC_KEY'],
-                                        str(os.environ['APPLEMUSIC_SECRET']).replace(r'\n', '\n'))
+                                        str(os.environ['APPLEMUSIC_SECRET']).replace(r'\n', '\n'),
+                                        access_token="AgtzQmglsdXAFX2GPW12mSpOr3rqIN/pd6/4zDE+RLWnk7UyhKJkbSizml7uYF7ROHSUPvBM6evJFbE5lP3GWgi38UHZ2c6xKZaKsMiDlGNJ/X60qU3gT/WjvGf7q1k4bJxqyrx/Rt7ZfiekXbt3nnvT/zzICHx+rxOFZBgt3nwNkP4ZpLrcIhRALWmq9g21gZZhga1bFPknykAyXFS0lIZ6WE6L7xH0yRLJ/ktzMx9oMXwNcQ=="
+                                        )
 
     def sample_code(self):
         resp = self.client.get_songs_by_isrc('US2U61726301')
+        pprint.pprint(resp)
+
+
+
+
+    def get_user_playlist(self):
+        resp = self.client.user_playlists(limit=5)
         pprint.pprint(resp)
--- src/debug.py
+++ src/debug.py
@@ -5,7 +5,21 @@ import urlshortener
 import gmusic
 import applemusic
 
-# Call methods from here to test them
-am = applemusic.AppleMusic()
+# testing socket
+import os
+import requests
+import webbrowser
 
-am.sample_code()
+os.environ['NO_PROXY'] = '127.0.0.1'
+
+webbrowser.open_new('http://127.0.0.1:8080')
+
+r = requests.get('http://127.0.0.1:8080/usertoken')
+print(r.content)
+
+# requests.put('http://127.0.0.1:8080/exit')
+
+# Call methods from here to test them
+# am = applemusic.AppleMusic()
+
+# am.get_user_playlist()
